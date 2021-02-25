4b93a0f558a6e08b9e722c8ce8fb26590797e35d
Daniel Wolfgang Malamud-Roam <dmalamudroam@gmail.com>
2021-02-24 16:11:12 -0800

    Game complete

--- game.js
+++ game.js
@@ -24,10 +24,10 @@ const coordinates = {
   '9': 81
 };
 
-let state = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
+let state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 let currentPlayer = 'x';
 
-String.prototype.replaceAt = function(index, replacement) {
+String.prototype.replaceAt = function (index, replacement) {
   return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
@@ -36,12 +36,98 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const updateState = (stringNum) => {
+  const num = Number(stringNum) - 1;
+  const array = Math.floor(num / 3);
+  const index = num % 3;
+
+  state[array][index] = currentPlayer;
+}
+
 const getInput = () => {
-rl.question(`${currentBoard}\n${currentPlayer} to move (enter move in form of a number): `, (answer) => {
-  let loc = coordinates[answer];
-  currentBoard = currentBoard.replaceAt(loc, currentPlayer);
-  console.log(currentBoard);
-});
+  rl.question(`${currentBoard}\n${currentPlayer} to move (enter move in form of a number): `, (answer) => {
+    let loc = coordinates[answer];
+    currentBoard = currentBoard.replaceAt(loc, currentPlayer);
+    console.log(currentBoard);
+    updateState(answer);
+    const isWin = checkWin(currentPlayer);
+    if (isWin) {
+      console.log(`${currentPlayer} WINS!!!`);
+      console.log('NEW GAME!')
+      currentBoard = startBoard;
+      currentPlayer = 'o';
+    }
+    if (currentPlayer === 'x') {
+      currentPlayer = 'o';
+    } else {
+      currentPlayer = 'x';
+    }
+    getInput();
+  });
+}
+
+//Check win
+var checkWin = (player) => {
+  isRowWin = checkAllRows(player);
+  isColWin = checkAllCols(player);
+  isMajorDiagWin = checkMajorDiag(player);
+  isMinorDiagWin = checkMinorDiag(player);
+  return isRowWin || isColWin || isMajorDiagWin || isMinorDiagWin;
+}
+//check rows
+var checkRow = (row, player) => {
+  counter = 0;
+  row.forEach((val) => {
+    if (val === player) {
+      counter++;
+    }
+  });
+  return counter === 3;
+}
+var checkAllRows = (player) => {
+  var result = false;
+  state.forEach((row) => {
+    result = checkRow(row, player) || result;
+  });
+  return result;
+}
+//Check Columns
+var checkCol = (col, player) => {
+  counter = 0;
+  state.forEach((row) => {
+    if (row[col] === player) {
+      counter++;
+    }
+  });
+  return counter === 3;
+}
+var checkAllCols = (player) => {
+  var result = false;
+  for (var i = 0; i < state.length; i++) {
+    result = checkCol(i, player) || result;
+  }
+  return result;
+}
+//Check diags
+var checkMajorDiag = (player) => {
+  var counter = 0;
+  for (var i = 0; i < state.length; i++) {
+    if (state[i][i] === player) {
+      counter++;
+    }
+  }
+  return counter === 3;
+}
+var checkMinorDiag = (player) => {
+  var counter = 0;
+  var row = 0;
+  for (var i = 2; i >= 0; i--) {
+    if (state[row][i] === player) {
+      counter++;
+    }
+    row++;
+  }
+  return counter === 3;
 }
 
 getInput();
